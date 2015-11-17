7428bba5995d25d22536f4cc2cb854dacd0fab62
gg2787 <278783384@qq.com>
2015-11-17 22:47:38 +0800

    增加小米推送（客户端）demo 漏提文件

---
+++ app/src/main/java/com/guoguo/pushClient/MyPushMessageReceiver.java
@@ -0,0 +1,78 @@
+package com.guoguo.pushClient;
+
+import android.content.Context;
+
+import com.guoguo.logic.log.Log;
+import com.guoguo.ui.toast.ShowToast;
+import com.xiaomi.mipush.sdk.ErrorCode;
+import com.xiaomi.mipush.sdk.MiPushClient;
+import com.xiaomi.mipush.sdk.MiPushCommandMessage;
+import com.xiaomi.mipush.sdk.MiPushMessage;
+import com.xiaomi.mipush.sdk.PushMessageReceiver;
+
+import java.util.List;
+
+/**
+ * 1、PushMessageReceiver是个抽象类，该类继承了BroadcastReceiver。
+ * 2、需要将自定义的DemoMessageReceiver注册在AndroidManifest.xml文件中 <receiver
+ * android:exported="true"
+ * android:name="com.xiaomi.mipushdemo.DemoMessageReceiver"> <intent-filter>
+ * <action android:name="com.xiaomi.mipush.RECEIVE_MESSAGE" /> </intent-filter>
+ * <intent-filter> <action android:name="com.xiaomi.mipush.ERROR" />
+ * </intent-filter> <intent-filter>
+ *  <action android:name="com.xiaomi.mipush.MESSAGE_ARRIVED" /></intent-filter>
+ *  </receiver>
+ * 3、DemoMessageReceiver的onReceivePassThroughMessage方法用来接收服务器向客户端发送的透传消息
+ * 4、DemoMessageReceiver的onNotificationMessageClicked方法用来接收服务器向客户端发送的通知消息，
+ * 这个回调方法会在用户手动点击通知后触发
+ * 5、DemoMessageReceiver的onNotificationMessageArrived方法用来接收服务器向客户端发送的通知消息，
+ * 这个回调方法是在通知消息到达客户端时触发。另外应用在前台时不弹出通知的通知消息到达客户端也会触发这个回调函数
+ * 6、DemoMessageReceiver的onCommandResult方法用来接收客户端向服务器发送命令后的响应结果
+ * 7、DemoMessageReceiver的onReceiveRegisterResult方法用来接收客户端向服务器发送注册命令后的响应结果
+ * 8、以上这些方法运行在非UI线程中
+ *
+ * @author mayixiang
+ */
+
+/**
+ * Created by Administrator on 2015/11/17.
+ */
+public class MyPushMessageReceiver extends PushMessageReceiver {
+    private static final String TAG = MyPushMessageReceiver.class.getSimpleName();
+    private String mRegId;
+
+    @Override
+    public void onReceivePassThroughMessage(Context context, MiPushMessage message) {
+        message.getContent();
+    }
+
+    @Override
+    public void onReceiveRegisterResult(Context context, MiPushCommandMessage message) {
+        String command = message.getCommand();
+        List<String> arguments = message.getCommandArguments();
+        String cmdArg1 = ((arguments != null && arguments.size() > 0) ? arguments.get(0) : null);
+        String log;
+        if (MiPushClient.COMMAND_REGISTER.equals(command)) {
+            if (message.getResultCode() == ErrorCode.SUCCESS) {
+                mRegId = cmdArg1;
+                ShowToast.showShortToast(context, "getRegId" + mRegId);
+                MyPushService.getInstance(context).onGetRegId(mRegId);
+                Log.error(TAG, "register_success");
+            } else {
+                Log.error(TAG, "register_fail");
+            }
+        } else {
+            Log.error(TAG, message.getReason());
+        }
+        //do sth
+    }
+
+    @Override
+    public void onCommandResult(Context context, MiPushCommandMessage message) {
+        String command = message.getCommand();
+        List<String> arguments = message.getCommandArguments();
+        String cmdArg1 = ((arguments != null && arguments.size() > 0) ? arguments.get(0) : null);
+        String cmdArg2 = ((arguments != null && arguments.size() > 1) ? arguments.get(1) : null);
+    }
+
+}
