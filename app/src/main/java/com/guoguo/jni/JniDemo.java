3678451a70c14c3df992ac5bc2c94208d73411e2
gg2787 <278783384@qq.com>
2015-12-03 20:35:37 +0800

    *testcommit

--- app/src/main/java/com/guoguo/jni/JniDemo.java
+++ app/src/main/java/com/guoguo/jni/JniDemo.java
@@ -2,11 +2,6 @@ package com.guoguo.jni;
 
 /**
  * Created by HP on 2015/12/1.
- *����һ�����java��
- * �������ʹ��javac��exe���� Java ���룬���JniDemo.class�ļ���
- * C:\Windows\system32>D:\Java\jdk1.7.0_79\bin\javac E:\GuoGuo\app\src\main\java\co
- m\guoguo\jni\jnidemo.java
- ������ʹ��javah.exe���� C/C++ ͷ�ļ�
  */
 public class JniDemo {
 //    static {
