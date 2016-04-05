9ca4d0198da683c2d0220e450a11e5b2758f7510
gg2787 <278783384@qq.com>
2016-04-05 11:28:33 +0800

    *图片的：缩放、裁剪、保存、人脸识别、文件名的处理

--- app/src/main/java/com/guoguo/wallpaperAndLock/Wallpaper.java
+++ app/src/main/java/com/guoguo/wallpaperAndLock/Wallpaper.java
@@ -1,17 +1,22 @@
 package com.guoguo.wallpaperAndLock;
 
 import android.app.Activity;
+import android.app.WallpaperInfo;
 import android.app.WallpaperManager;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.media.FaceDetector;
 import android.media.MediaScannerConnection;
-import android.os.Environment;
-import android.os.Handler;
 import android.text.TextUtils;
 
 import com.guoguo.R;
+import com.guoguo.ui.toast.ShowToast;
 import com.guoguo.utils.FileUtils;
 import com.guoguo.utils.UIutils;
 
@@ -19,8 +24,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2016/3/25.
@@ -33,22 +37,23 @@ public class Wallpaper {
     public static final int SAVE_RESULT_SUCCESS = 2;
     public static final int SAVE_RESULT_FAIL = 3;
 
+    private static final int MAX_FACES = 3;
+
     public void changeWall(final Activity activity) {
 
-        final Bitmap  bitmap = UIutils.loadBitmap(activity.getApplicationContext(), R.drawable.small2);
+        final Bitmap  bitmap = UIutils.loadBitmap(activity.getApplicationContext(), R.drawable.wall_face);
+
+//        faceDetect(bitmap);
 
         final Bitmap bitmap2 = imageScaleAndCrop(activity, bitmap);
 
         new Thread(new Runnable() {
             public void run() {
-                //execute the task
-                //do something
+
                 setWallpaper(activity.getApplicationContext(), bitmap2);
                 bitmap2.recycle();
             }
         }).start();
-
-//        setWallpaper(context, bitmap);
     }
 
     private void setWallpaper (Context context, Bitmap bitmap) {
@@ -67,8 +72,7 @@ public class Wallpaper {
         if (null == activity || null == bitmapSrc) {
             return null;
         }
-        getWallpaper(activity.getApplicationContext());
-        //计算屏幕的宽高比
+
         int[] nScreenSize = UIutils.getScreenSize(activity);
         if (nScreenSize.length < 2) {
             return null;
@@ -76,36 +80,77 @@ public class Wallpaper {
         int nScreenWidth = nScreenSize[0];
         int nScreenHeight = nScreenSize[1];
 
+//        int[] minSize = getMinWallpaperAttr(activity);
+//        if (null != minSize) {
+//            nScreenWidth = minSize[0];
+//            nScreenHeight = minSize[1];
+//        }
+
         int nSrcWidth = bitmapSrc.getWidth();
         int nSrcHeight = bitmapSrc.getHeight();
 
-        //计算出缩放,先裁剪 后缩放
-        float fScale = Math.max((float) nScreenWidth / (float) nSrcWidth, (float) nScreenHeight / (float) nSrcHeight);
-        Matrix matrix = new Matrix();
-        float fScaleMatrix = (float)Math.ceil(fScale * 100) / 100;
-        matrix.setScale(fScaleMatrix, fScaleMatrix);
-
-        int nDstX = (int)(Math.min(nSrcWidth, Math.ceil(nScreenWidth / fScale)));
-        int nDstY = (int)(Math.min(nSrcHeight, Math.ceil(nScreenHeight / fScale)));
-        int nStartX = (int)Math.floor((nSrcWidth - nDstX) / 2);
-        int nStartY = (int)Math.floor((nSrcHeight - nDstY) / 2);
-
-        //裁剪图片
-        Bitmap bitmapDstScale = null;
-        try {
-            bitmapDstScale = Bitmap.createBitmap(bitmapSrc, nStartX, nStartY,
-                    nDstX, nDstY, matrix, false);
-        } catch (IllegalArgumentException e) {
-            return null;
+        if (nSrcWidth == nScreenWidth && nSrcHeight == nScreenHeight) {
+            return bitmapSrc;
         }
 
-        Bitmap bitmapDstAdjust = Bitmap.createBitmap(bitmapDstScale, 0, 0,
-                nScreenWidth, nScreenHeight);
-        bitmapDstScale.recycle();
+        int nCenterX = nSrcWidth / 2; //可能为0
+        PointF face = faceDetect(bitmapSrc);
+        if (face != null) {
+            nCenterX = (int)face.x;
+        }
 
-        saveWallpaper(activity.getApplicationContext(), bitmapDstAdjust, "wall1", "GuoguoWall", null);
+        //计算出缩放,先裁剪 后缩放
+        float fScale = Math.max((float) nScreenWidth / (float) nSrcWidth, (float) nScreenHeight / (float) nSrcHeight);
+        float fScaleMatrix = (float)Math.ceil(fScale * 100) / 100;
 
-        return bitmapDstAdjust;
+        int nSrcX = (int)(Math.min(nSrcWidth, nScreenWidth / fScale));
+        int nSrcY = (int)(Math.min(nSrcHeight, nScreenHeight / fScale));
+//        int nSrcStartX = (int)Math.floor((nSrcWidth - nSrcX) / 2);
+        int nSrcStartX = getStartX(nCenterX, nSrcWidth, nSrcX);
+        int nSrcStartY = (int)Math.floor((nSrcHeight - nSrcY) / 2);
+
+        Bitmap bitmapDst = bitmapScaleAndCrop(bitmapSrc, nSrcX, nSrcY,
+                nSrcStartX, nSrcStartY, nScreenWidth, nScreenHeight, Bitmap.Config.ARGB_4444);
+
+//        saveWallpaper(activity.getApplicationContext(), bitmapDstAdjust, "wall1", "GuoguoWall", null);
+
+        return bitmapDst;
+    }
+
+    private int getStartX(int nCenterX, int nSrcX, int nDstX) {
+        int nStart = nCenterX - nDstX / 2 - 1;
+        int nEnd = nCenterX + nDstX / 2 + 1;
+        if (nEnd > nSrcX) {
+            nStart = nSrcX - nDstX;
+        }
+        if (nStart < 0) {
+            nStart = 0;
+        }
+        return nStart;
+    }
+
+    public static int[] getMinWallpaperAttr(Context context) {
+        if (null == context) {
+            return null;
+        }
+        int nWallWidth, nWallHeight;
+
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(context);
+        Drawable wallpaperDrawable = wallpaperManager.getDrawable();
+        if (wallpaperDrawable != null) {
+            nWallWidth = wallpaperDrawable.getMinimumWidth();
+            nWallHeight = wallpaperDrawable.getMinimumHeight();
+        } else {
+            nWallWidth = wallpaperManager.getDesiredMinimumWidth();
+            nWallHeight = wallpaperManager.getDesiredMinimumHeight();
+        }
+
+        int[] wallSize = new int[2];
+        wallSize[0] = nWallWidth;
+        wallSize[1] = nWallHeight;
+
+        WallpaperInfo info = wallpaperManager.getWallpaperInfo();
+        return wallSize;
     }
 
     private void getWallpaper (Context context) {
@@ -114,15 +159,23 @@ public class Wallpaper {
         int nHeight3 = wallpaperManager.getDesiredMinimumHeight();
 
         final Drawable wallpaperDrawable = wallpaperManager.getDrawable();
+        if (wallpaperDrawable == null) {
+            return;
+        }
         int nWidth = wallpaperDrawable.getIntrinsicWidth();
         int nHeight = wallpaperDrawable.getIntrinsicHeight();
 
         int nWidth1 = wallpaperDrawable.getMinimumWidth();
         int nHeight2 = wallpaperDrawable.getMinimumHeight();
+
+        ShowToast.showLongToast(context,
+                "DesiredMinimumWidth_" + String.valueOf(nWidth3) + "DesiredMinimumHeight_" + String.valueOf(nHeight3)
+                        + "getIntrinsicWidth" + String.valueOf(nWidth) + "getIntrinsicHeight" + String.valueOf(nHeight)
+                        + "getMinimumWidth" + String.valueOf(nWidth1) + "getMinimumHeight" + String.valueOf(nHeight2));
     }
 
     public static void saveWallpaper(Context context, Bitmap bitmap, String strFileName, String strDirName, WallpaperSaveListener listener) {
-        if (null == context || null == bitmap || TextUtils.isEmpty(strFileName) || TextUtils.isEmpty(strDirName)) {
+        if (null == context || null == bitmap || bitmap.isRecycled() || TextUtils.isEmpty(strFileName) || TextUtils.isEmpty(strDirName)) {
             return;
         }
         int nResult = SAVE_RESULT_FAIL;
@@ -182,18 +235,115 @@ public class Wallpaper {
     }
 
     private static String regularFileName(String strFileName) {
-        strFileName = "/123.5";
         StringBuilder sbName = new StringBuilder(strFileName);
         int nIndexSlash = strFileName.lastIndexOf('/');
         if (nIndexSlash >= 0 && nIndexSlash < strFileName.length()) {
             sbName.delete(0, nIndexSlash + 1);
         }
 
-        int nIndexDot = sbName.toString().lastIndexOf('.'); //从0开始
-        if (nIndexDot >= 0 && nIndexDot < strFileName.length()) {
+        int nIndexDot = sbName.toString().lastIndexOf('.');
+        if (nIndexDot > 0 && nIndexDot < strFileName.length() - 1) {
             sbName.delete(nIndexDot, sbName.length());
         }
 
+        if (sbName.length() == 0) {
+            sbName.append(System.currentTimeMillis());
+        }
         return sbName.toString();
     }
+
+    private static PointF faceDetect(Bitmap bitmapSrc) {
+        if (null == bitmapSrc || bitmapSrc.isRecycled()) {
+            return null;
+        }
+        try {
+//            Matrix matrix = new Matrix();
+//            matrix.setScale(0.5f, 0.5f);
+//            Bitmap bitmapSmall = Bitmap.createBitmap(bitmapSrc, 0, 0, bitmapSrc.getWidth(), bitmapSrc.getHeight(), matrix, false);
+//
+////            Bitmap.createBitmap()
+//
+//            Bitmap bitmapFace = bitmapSmall;
+//            if (bitmapFace.getConfig() != Bitmap.Config.RGB_565) {
+//                bitmapFace = bitmapSmall.copy(Bitmap.Config.RGB_565, true);
+//                bitmapSmall.recycle();
+//                bitmapSmall = null;
+//            }
+
+            Bitmap bitmapFace = bitmapScaleAndCrop(bitmapSrc, bitmapSrc.getWidth(), bitmapSrc.getHeight(), 0, 0, (int) (bitmapSrc.getWidth() * 0.5f), (int) (bitmapSrc.getHeight() * 0.5f), Bitmap.Config.RGB_565);
+            if (null == bitmapFace) {
+                return null;
+            }
+
+            FaceDetector faceDet = new FaceDetector(bitmapFace.getWidth(), bitmapFace.getHeight(), 1);
+            FaceDetector.Face[] faceList = new FaceDetector.Face[1];
+            faceDet.findFaces(bitmapFace, faceList);
+            bitmapFace.recycle();
+
+            FaceDetector.Face face = faceList[0];
+            if (face != null) {
+                PointF pf = new PointF();
+                face.getMidPoint(pf);
+                pf.set(pf.x * 2, pf.y * 2);
+                return pf;
+            }else {
+                return null;
+            }
+
+        } catch (OutOfMemoryError e) {
+            System.gc();
+        }
+        return null;
+    }
+
+    private static ArrayList<PointF> faceDetectList(Bitmap bitmapSrc) {
+        Bitmap tmpBmp = bitmapSrc.copy(Bitmap.Config.RGB_565, true);
+
+        FaceDetector faceDet = new FaceDetector(tmpBmp.getWidth(), tmpBmp.getHeight(), MAX_FACES);
+        FaceDetector.Face[] faceList = new FaceDetector.Face[MAX_FACES];
+        faceDet.findFaces(tmpBmp, faceList);
+
+        ArrayList<PointF> arrFaceList = new ArrayList<>();
+
+        for (int i = 0; i < faceList.length; i++) {
+            FaceDetector.Face face = faceList[i];
+            if (face != null) {
+                PointF pf = new PointF();
+                //getMidPoint(PointF point);
+                //Sets the position of the mid-point between the eyes.
+                face.getMidPoint(pf);
+//                RectF r = new RectF();
+//                r.left = pf.x - face.eyesDistance() / 2;
+//                r.right = pf.x + face.eyesDistance() / 2;
+//                r.top = pf.y - face.eyesDistance() / 2;
+//                r.bottom = pf.y + face.eyesDistance() / 2;
+//                faceRects[i] = r;
+//                detectedFaces++
+                arrFaceList.add(pf);
+            }
+        }
+        return arrFaceList;
+    }
+
+    private static Bitmap bitmapScaleAndCrop(Bitmap bitmapSrc, int srcWidth, int srcHeight, int srcOffsetX, int srcOffsetY, int dstWidth, int dstHeight, Bitmap.Config config) {
+        if (null == bitmapSrc || bitmapSrc.isRecycled()) {
+            return null;
+        }
+        try {
+            Bitmap bitmapDst = Bitmap.createBitmap(dstWidth, dstHeight, config);
+            Rect rectDst = new Rect(0, 0, dstWidth, dstHeight);
+            Rect rectSrc = new Rect(srcOffsetX, srcOffsetY, Math.min(srcOffsetX + srcWidth, bitmapSrc.getWidth()), Math.min(srcHeight + srcOffsetY, bitmapSrc.getHeight()));
+
+            Canvas canvas = new Canvas(bitmapDst);
+            Paint p = new Paint();
+            p.setFilterBitmap(true);
+            canvas.drawBitmap(bitmapSrc, rectSrc, rectDst, p);
+            canvas = null;
+            return bitmapDst;
+        } catch (OutOfMemoryError e) {
+            System.gc();
+            return null;
+        }
+    }
+
 }
